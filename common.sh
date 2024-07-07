3d843ae7a5700b40c0c395b7e50ee7d4c1399ed7
Vheekey <vicformidable@gmail.com>
2024-07-07 18:48:22 +0200

    adds folders

--- common.sh
+++ common.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-DATA_FILE="apartments.json"
+DATA_FILE="sites/apartments.json"
 TEMP_FILE="apartments_bfgen.txt"
 SUBJECT="Available Apartments"
 EMAIL_SCRIPT="send_email.sh"
 MIN_PRICE=14001
-BFGEN_CRAWLER="bfgen_crawler_node.js"
+BFGEN_CRAWLER="sites/bfgen_crawler_node.js"
 RECIPIENT="vheey01@gmail.com"
 EMAIL_LOG="logs/email_log.txt"
 LOG_FILE="logs/log.txt"
